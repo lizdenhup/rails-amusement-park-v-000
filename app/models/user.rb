e724279b34816d2123a4be5762de156656f86c8d
lizdenhup <lizdenhup@gmail.com>
2016-10-29 12:23:16 -0500

    wrote migrations, models, am passing all user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_secure_password 
+  has_many :rides
+  has_many :attractions, through: :rides 
+
+  validates_presence_of :name, :password, :happiness, :nausea, :height, :tickets
+
+  def mood
+    if self.nausea < self.happiness 
+      mood = "happy"
+    elsif self.happiness < self.nausea
+      mood = "sad"
+    end 
+  end 
+
 end
--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,8 @@
 class CreateRides < ActiveRecord::Migration
-  # Write your migrations here
+  def change
+    create_table :rides do |t|
+      t.references :user, index: true
+      t.references :attraction, index: true
+    end 
+  end
 end
