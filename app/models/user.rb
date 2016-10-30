428d65c756ed09e1cc3a713400662bc49255b118
lizdenhup <lizdenhup@gmail.com>
2016-10-30 11:42:28 -0500

    passing all attraction model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides 
 
-  validates_presence_of :name, :password, :happiness, :nausea, :height, :tickets
+  validates_presence_of :name, :password
 
   def mood
     if self.nausea < self.happiness 
