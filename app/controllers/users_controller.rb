ffc0600e415bff0b5efa6597ace95508b98f00f9
lizdenhup <lizdenhup@gmail.com>
2016-11-03 21:20:02 -0500

    passing user signup specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to user_path(@user)
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user), notice: "Welcome to the theme park!"
+    else 
+      redirect_to new_user_path
+    end 
   end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end 
 
   def update
     @user = User.find_by_id(params[:id])
@@ -15,7 +27,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
     end 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides 
 
-  validates_presence_of :name, :password, :tickets 
+  validates_presence_of :name, :password 
 
   def mood
     if self.nausea < self.happiness 
